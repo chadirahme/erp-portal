4e853578c471ee7637289e290f1a641f5ad6e427
crahme <eng.chadi@gmail.com>
2018-09-19 17:19:39 -0400

    add form module

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { routerTransition } from '../router.animations';
 
@@ -9,9 +10,20 @@ import { routerTransition } from '../router.animations';
     animations: [routerTransition()]
 })
 export class LoginComponent implements OnInit {
-    constructor(public router: Router) {}
+    form: FormGroup;                    // {1}
+    constructor(
+        public router: Router,
+        private fb: FormBuilder
+    ) {}
 
-    ngOnInit() {}
+    // ngOnInit() {}
+    ngOnInit() {
+        this.form = this.fb.group({     // {5}
+            userName: ['chadi', Validators.required],
+             password: ['', Validators.required]
+        });
+    }
+
 
     onLoggedin() {
         localStorage.setItem('isLoggedin', 'true');
